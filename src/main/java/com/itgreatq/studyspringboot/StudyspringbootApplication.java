ae1230345a6c7886b36f750870a99bde201cf0c7
weijieqiu <qiuweijiecn@foxmail.com>
2020-06-09 20:59:42 +0800

    初始化 springboot 项目

---
+++ src/main/java/com/itgreatq/studyspringboot/StudyspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.itgreatq.studyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itgreatq/studyspringboot/StudyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itgreatq.studyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
